98a214f1befaac3ce2f82f1faa5123645a1f4e9c
yashnmori <76810986+yashnmori@users.noreply.github.com>
2021-01-02 15:59:58 +0530

    Update Multiple_MF_StocksPortfolio.py

--- Multiple_MF_StocksPortfolio.py
+++ Multiple_MF_StocksPortfolio.py
@@ -1,6 +1,4 @@
-
 # coding: utf-8
-
 #Necessary Libs
 import requests
 import bs4
@@ -9,18 +7,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pandas import ExcelWriter
 
-
-
-
-
 #Portfolio Holdings URL for all 5 MF Portfolio
 url_1 = requests.get('https://www.moneycontrol.com/mutual-funds/axis-small-cap-fund-regular-plan/portfolio-holdings/MAA314')
 url_2 = requests.get('https://www.moneycontrol.com/mutual-funds/kotak-small-cap-fund-regular-plan/portfolio-holdings/MKM073')
 url_3 = requests.get('https://www.moneycontrol.com/mutual-funds/dsp-small-cap-fund-regular-plan/portfolio-holdings/MDS076')
 url_4 = requests.get('https://www.moneycontrol.com/mutual-funds/l-t-emerging-businesses-fund-regular-plan/portfolio-holdings/MCC490')
 url_5 = requests.get('https://www.moneycontrol.com/mutual-funds/nippon-india-small-cap-fund/portfolio-holdings/MRC587')
-
-
 
 #Creating BS$ object using lxml parser
 soup_1 = bs4.BeautifulSoup(url_1.text,'lxml')
@@ -29,7 +21,6 @@ soup_3 = bs4.BeautifulSoup(url_3.text,'lxml')
 soup_4 = bs4.BeautifulSoup(url_4.text,'lxml')
 soup_5 = bs4.BeautifulSoup(url_5.text,'lxml')
 
-
 #getting titles from all webpages
 a = '_'.join(soup_1.find('title').text.split()[:4])
 b = '_'.join(soup_2.find('title').text.split()[:4])
@@ -37,19 +28,10 @@ c = '_'.join(soup_3.find('title').text.split()[:4])
 d = '_'.join(soup_4.find('title').text.split()[:4])
 e = '_'.join(soup_5.find('title').text.split()[:4])
 
-
-
-
-
 #Creating a list of bs4 objects.
 soups = [soup_1,soup_2,soup_3,soup_4,soup_5]
 
-
-
-
-
 #Getting table from all URL with given ID and Storing it into List
-
 test_list=[]
 data_content=[]
 
@@ -66,23 +48,18 @@ for x in soups:
 data_content
 
 
-
+#Length of new list.
 print(len(data_content))
-
-
 
 #Converting List into Dataframe
 dataset = pd.DataFrame(data_content)
 dataset
-
-
 
 #Dataframe Transpose
 dt = pd.DataFrame(dataset.T)
 #Adding Titles to New Dataframe
 dt.columns = [a,b,c,d,e]
 dt
-
 
 #Saving Dataframe to Excel.
 writer = ExcelWriter('F:/New folder/PythonExport.xlsx')
